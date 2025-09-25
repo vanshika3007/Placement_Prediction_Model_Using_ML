2d93c0a4c802ea99c3eec9b6227f2bb753c444d6
vanshika3007 <vanshikabhardwaj30082005@gmail.com>
2025-09-25 13:24:45 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
 # Start the Flask application with Gunicorn from backend root
-exec gunicorn --bind 0.0.0.0:$PORT --workers 1 app:app
+#!/bin/bash
+gunicorn app:app --workers=4 --bind=0.0.0.0:$PORT
